fcadba50752e04082efed32b137c63294af7a455
Aloxan12 <kvakvaaak@mail.ru>
2021-09-10 11:56:49 +0300

    refresh(2)

--- src/utils/changeColor.js
+++ src/utils/changeColor.js
@@ -1,45 +1,40 @@
-export const markObj = (arr) => {
-    let count = 0;
-    let notWorkCount = 3;
-    const items = [];
-
+export
+const markObj = (arr) => {
     const newArr = arr.reduce((acc, item) => {
-        return [...acc, { id: item.id, time: item.from, count: 'plus'}, {id: item.id, time: item.to, count: 'minus'}]
+        return [
+            ...acc,
+            { id: item.id, time: item.from, count: "plus" },
+            { id: item.id, time: item.to, count: "minus" }
+        ];
     }, []);
 
     const sortArr = newArr.sort((a, b) => {
         return new Date(a.time) - new Date(b.time);
     });
 
-    sortArr.forEach((item, index) => {
-        if (count < 3) {
-            if (item.count === 'plus') {
-                count += 1;
+    const a = sortArr.reduce(
+        (acc, item, index) => {
+            if (acc.count + 1 < 3 && item.count === "plus") {
+                console.log({ ...acc, item: item.count, count: acc.count + 1, index });
+                return { ...acc, item: item.count, count: acc.count + 1 };
+            } else if (acc.count - 1 < 3 && item.count === "minus") {
+                acc.items.push(sortArr[index - 1].id);
+                console.log({ ...acc, item: item.count, count: acc.count - 1, index });
+                return {
+                    ...acc,
+                    item: item.count,
+                    count: acc.count - 1,
+                    items: acc.items
+                };
+            } else if (acc.count + 1 >= 3 && item.count === "plus") {
+                return { ...acc, item: item.count, count: acc.count + 1 };
             } else {
-                count -= 1;
+                return { ...acc, item: item.count, count: acc.count - 1 };
             }
-        }
+        },
+        { count: 0, item: "", items: [] }
+    );
 
-        if (count >= 3) {
-            if (notWorkCount >= 3) {
-                if (item.count === 'plus') {
-                    notWorkCount += 1;
-                } else {
-                    notWorkCount -= 1;
-                }
-            }
-
-            if (notWorkCount < 3) {
-                sortArr[index-1].count === 'minus' && items.push(sortArr[index-1].id);
-                if (item.count === 'plus') {
-                    notWorkCount += 1;
-                } else {
-                    notWorkCount -= 1;
-                }
-            }
-        }
-    });
-
-    return [...new Set(items)];
+    return a.items;
 };
 
