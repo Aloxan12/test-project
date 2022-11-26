43929d4d80830e76f7970988c12d39eeb619844d
Aloxan12 <kvakvaaak@mail.ru>
2022-11-26 10:58:57 +0300

    test for thunk(1)

--- src/BLL/mainReducer.thunk.test.js
+++ src/BLL/mainReducer.thunk.test.js
@@ -1,14 +1,7 @@
 import {setEmployeesT, setWorklogTC} from "./mainReducer";
-// import {getEmployees, getWorklog} from "../DAL/api";
-// jest.mock("../DAL/api")
 
-// const getEmployeesMock = getEmployees
-// const getWorklogMock = getWorklog
-
-//getEmployeesMock.mockReturnValue()
-test('setEmployeesTC',async ()=>{
+test('setEmployeesT',async ()=>{
     const thunk = setEmployeesT();
-
     const dispatchMock = jest.fn();
 
     await thunk(dispatchMock);
@@ -17,7 +10,6 @@ test('setEmployeesTC',async ()=>{
 })
 test('setWorklogTC',async ()=>{
     const thunk = setWorklogTC();
-
     const dispatchMock = jest.fn();
 
     await thunk(dispatchMock);
